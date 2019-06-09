b0016a4df654334d5934c088b3cb269f5463611b
Keshari Nandan <keshari.nandan2@gmail.com>
2019-06-10 00:00:55 +0530

    Updated Model Fillable Fields

--- src/Models/TheRequestLog.php
+++ src/Models/TheRequestLog.php
@@ -3,8 +3,22 @@
 namespace TheNandan\TheLogger\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TheRequestLog extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'start_time',
+        'end_time',
+        'ip_address',
+        'url',
+        'method',
+        'input',
+        'output',
+    ];
 }
